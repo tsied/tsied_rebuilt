bde79b5ec97edff0faacc1349b9b668410cad5a5
fkueje001 <527533745@qq.com>
2016-04-11 16:55:05 +0700

    去掉模板测试方法，增加测试类

--- src/test/ElasticClient.java
+++ src/test/ElasticClient.java
@@ -1,4 +1,4 @@
-package com.elk.es;
+package test;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -12,6 +12,7 @@ import java.util.Map;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,7 @@ import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.elk.es.Script;
 import com.elk.utils.JsonUtil;
 import com.elk.utils.StringUtil;
 import com.elk.utils.TemplateUtil;
@@ -35,10 +37,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class ElasticClient {
 
+	//private static TransportClient client = null;
+
 	private static Logger log = LoggerFactory.getLogger(ElasticClient.class);
 	
 	@Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
+	/**
+	 * init client instance
+	 * @return
+	 */
+	/*public  ElasticClient() {
+		try {
+			InputStream in = new BufferedInputStream (new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("").getPath()+"/resource/esConfig.properties"));
+			Properties properties = new Properties();
+			properties.load(in);
+			Settings settings = Settings.settingsBuilder().put("cluster.name", properties.get("clusterName")).build();
+			 client = TransportClient.builder().settings(settings).build().addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(properties.get("esip").toString()), Integer.parseInt(properties.get("esport").toString())));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}*/
+
+	/**
+	 * close client
+	 */
+	/*public void closed() {
+		if (null != client) {
+			client.close();
+			client = null;
+		}
+	}*/
 
 	/**
 	 * exec template(search,delete,insert....)
@@ -110,4 +139,28 @@ public class ElasticClient {
 		return resultMap;
 	}
 	
+	
+	public static void main(String[] args) throws ParseException {
+		ElasticClient esClient = new ElasticClient();
+		String templatePath = Thread.currentThread().getContextClassLoader().getResource("resource/template/es").getPath()+"/register-user.customcache";
+		String content = esClient.readFile(templatePath);
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Script script = new Script("pay_s","pay_game",Long.parseLong("1451577600000"),Long.parseLong("1483199999999"));
+		//Script script = new Script("pay_stats","pay_test",Long.parseLong("1451577600000"),Long.parseLong("1483199999999"),"/static/js/server.js");
+		//script.setDateRange("@timestamp");
+	//	long startTime = format.parse("2016-03-01").getTime() / 1;
+		//long endTime   = format.parse("2016-03-31").getTime() / 1;
+	//	System.out.println(startTime +"---------------------------"+DateUtils.getTime(format.parse("2016-03-01")));
+		Map<String, String> resultMap = esClient.execQuery(content,script);
+		resultMap.put("templateName", "register-user.ftl");
+		String data = new TemplateUtil().formatData(resultMap);
+		System.out.println(data);
+		/*SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar ca = Calendar.getInstance();//得到一个Calendar的实例 
+		ca.setTime(new Date()); //设置时间为当前时间 
+		ca.add(Calendar.YEAR, -1); //年份减1 
+		Date lastMonth = ca.getTime();
+		System.out.println(DateUtils.formatDate(lastMonth));
+		System.out.println(DateUtils.getCurrentDate());*/
+	}
 }
